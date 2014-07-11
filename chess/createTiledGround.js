88abf36b2252fe704561aeec0972dad3274ff063
celian <celian.garcia@utbm.fr>
2014-07-11 14:50:57 +0200

    createTiledGroud updated from master

--- chess/createTiledGround.js
+++ chess/createTiledGround.js
@@ -10,56 +10,46 @@ var createTiledGround = function (xmin, zmin, xmax, zmax, precision, subdivision
     precision.h = precision.h || 1;
     precision.w = precision.w || 1;
 
-    var tileSize = {
-        'w' : (xmax - xmin) / subdivisions.w,
-        'h' : (zmax - zmin) / subdivisions.h
+    var totalSubdiv = {
+        'w' : (subdivisions.w * precision.w),
+        'h' : (subdivisions.h * precision.h),
     };
 
-    for (tileRow = 0; tileRow < subdivisions.h; tileRow++) {
-        for (tileCol = 0; tileCol < subdivisions.w; tileCol++) {
-            applyTile(
-                xmin + (tileCol * tileSize.w),
-                zmin + (tileRow * tileSize.h),
-                xmin + ((tileCol + 1) * tileSize.w),
-                zmin + ((tileRow + 1) * tileSize.h)
-            );
+    // Position normals & uvs
+    var position = new BABYLON.Vector3.Zero();
+    var normal = new BABYLON.Vector3(0, 1.0, 0);
+    for (row = 0; row <= totalSubdiv.h; row++) {
+        position.z = (row * (zmax - zmin)) / totalSubdiv.h + zmin;
+        for (col = 0; col <= totalSubdiv.w; col++) {
+            position.x = (col * (xmax - xmin)) / totalSubdiv.w + xmin;
+            position.y = 0;
+
+            positions.push(position.x, position.y, position.z);
+            normals.push(normal.x, normal.y, normal.z);
+            uvs.push(col / precision.w, row / precision.h);
         }
     }
 
-    function applyTile (xTileMin, zTileMin, xTileMax, zTileMax) {
-        // Indices
-        var base = positions.length/3;
-        var rowLength = precision.w + 1;
-        for (row = 0; row < precision.h; row++) {
-            for (col = 0; col < precision.w; col++) {
-                var square = [
-                    base +  col + row * rowLength,
-                    base + (col + 1) + row * rowLength,
-                    base + (col + 1) + (row + 1) * rowLength,
-                    base +  col + (row + 1) * rowLength
-                ];
+    // Indices
+    var rowLength = totalSubdiv.w + 1;
+    for (tileRow = 0; tileRow < subdivisions.h; tileRow++) {
+        for (tileCol = 0; tileCol < subdivisions.w; tileCol++) {
+            for (row = 0; row < precision.h; row++) {
+                for (col = 0; col < precision.w; col++) {
+                    var square = [
+                        col + row * rowLength + tileCol * precision.w + tileRow * rowLength * precision.h,
+                        (col + 1) + row * rowLength + tileCol * precision.w + tileRow * rowLength * precision.h ,
+                        (col + 1) + (row + 1) * rowLength + tileCol * precision.w + tileRow * rowLength * precision.h,
+                        col + (row + 1) * rowLength + tileCol * precision.w + tileRow * rowLength * precision.h
+                    ];
 
-                indices.push(square[1]);
-                indices.push(square[2]);
-                indices.push(square[3]);
-                indices.push(square[0]);
-                indices.push(square[1]);
-                indices.push(square[3]);
-            }
-        }
-
-        // Position, normals and uvs
-        var position = new BABYLON.Vector3.Zero();
-        var normal = new BABYLON.Vector3(0, 1.0, 0);
-        for (row = 0; row <= precision.h; row++) {
-            position.z = (row * (zTileMax - zTileMin)) / precision.h + zTileMin;
-            for (col = 0; col <= precision.w; col++) {
-                position.x = (col * (xTileMax - xTileMin)) / precision.w + xTileMin;
-                position.y = 0;
-
-                positions.push(position.x, position.y, position.z);
-                normals.push(normal.x, normal.y, normal.z);
-                uvs.push(col / precision.w, row / precision.h);
+                    indices.push(square[1]);
+                    indices.push(square[2]);
+                    indices.push(square[3]);
+                    indices.push(square[0]);
+                    indices.push(square[1]);
+                    indices.push(square[3]);
+                }
             }
         }
     }
