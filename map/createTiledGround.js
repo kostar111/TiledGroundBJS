a185216a23a3d7c028f48bfde168b2e08750a63e
celian <celian.garcia@utbm.fr>
2014-07-11 11:55:28 +0200

    come back to previous version

--- map/createTiledGround.js
+++ map/createTiledGround.js
@@ -10,51 +10,59 @@ var createTiledGround = function (xmin, zmin, xmax, zmax, precision, subdivision
     precision.h = precision.h || 1;
     precision.w = precision.w || 1;
 
-    var totalSubdiv = {
-        'w' : (subdivisions.w * precision.w),
-        'h' : (subdivisions.h * precision.h),
+    var tileSize = {
+        'w' : (xmax - xmin) / subdivisions.w,
+        'h' : (zmax - zmin) / subdivisions.h
     };
 
-    // Position & normals
-    var position = new BABYLON.Vector3.Zero();
-    var normal = new BABYLON.Vector3(0, 1.0, 0);
-    for (row = 0; row <= totalSubdiv.h; row++) {
-        position.z = (row * (zmax - zmin)) / totalSubdiv.h + zmin;
-        for (col = 0; col <= totalSubdiv.w; col++) {
-            position.x = (col * (xmax - xmin)) / totalSubdiv.w + xmin;
-            position.y = 0;
-
-            positions.push(position.x, position.y, position.z);
-            normals.push(normal.x, normal.y, normal.z);
+    for (tileRow = 0; tileRow < subdivisions.h; tileRow++) {
+        for (tileCol = 0; tileCol < subdivisions.w; tileCol++) {
+            applyTile(
+                xmin + (tileCol * tileSize.w),
+                zmin + (tileRow * tileSize.h),
+                xmin + ((tileCol + 1) * tileSize.w),
+                zmin + ((tileRow + 1) * tileSize.h)
+            );
         }
     }
 
-    // Indices & uvs
-    var rowLength = totalSubdiv.w + 1;
-    for (tileRow = 0; tileRow < subdivisions.h; tileRow++) {
-        for (tileCol = 0; tileCol < subdivisions.w; tileCol++) {
-            for (row = 0; row < precision.h; row++) {
-                for (col = 0; col < precision.w; col++) {
-                    var square = [
-                        col + row * rowLength + tileCol * precision.w + tileRow * rowLength * 2,
-                        (col + 1) + row * rowLength + tileCol * precision.w + tileRow * rowLength * 2 ,
-                        (col + 1) + (row + 1) * rowLength + tileCol * precision.w + tileRow * rowLength * 2,
-                        col + (row + 1) * rowLength + tileCol * precision.w + tileRow * rowLength * 2
-                    ];
+    function applyTile (xTileMin, zTileMin, xTileMax, zTileMax) {
+        // Indices
+        var base = positions.length/3;
+        var rowLength = precision.w + 1;
+        for (row = 0; row < precision.h; row++) {
+            for (col = 0; col < precision.w; col++) {
+                var square = [
+                    base +  col + row * rowLength,
+                    base + (col + 1) + row * rowLength,
+                    base + (col + 1) + (row + 1) * rowLength,
+                    base +  col + (row + 1) * rowLength
+                ];
 
-                    indices.push(square[1]);
-                    indices.push(square[2]);
-                    indices.push(square[3]);
-                    indices.push(square[0]);
-                    indices.push(square[1]);
-                    indices.push(square[3]);
+                indices.push(square[1]);
+                indices.push(square[2]);
+                indices.push(square[3]);
+                indices.push(square[0]);
+                indices.push(square[1]);
+                indices.push(square[3]);
+            }
+        }
 
-                    uvs.push(col / precision.w, row / precision.h);
-                }
+        // Position, normals and uvs
+        var position = new BABYLON.Vector3.Zero();
+        var normal = new BABYLON.Vector3(0, 1.0, 0);
+        for (row = 0; row <= precision.h; row++) {
+            position.z = (row * (zTileMax - zTileMin)) / precision.h + zTileMin;
+            for (col = 0; col <= precision.w; col++) {
+                position.x = (col * (xTileMax - xTileMin)) / precision.w + xTileMin;
+                position.y = 0;
+
+                positions.push(position.x, position.y, position.z);
+                normals.push(normal.x, normal.y, normal.z);
+                uvs.push(col / precision.w, row / precision.h);
             }
         }
     }
-
 
     // Result
     var vertexData = new BABYLON.VertexData();
